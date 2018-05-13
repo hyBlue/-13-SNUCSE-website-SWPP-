4835299496881f3d4cfd66cbfa47e0be7098bcf5
hongsukchoi <redstonepo@gmail.com>
2018-05-13 17:38:17 +0900

    머지오류 정리

--- CSE_frontend/src/actions/index.js
+++ CSE_frontend/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_NOTICES = 'fetch_notices';
 export const FETCH_NOTICE = 'fetch_notice';
 export const CREATE_NOTICE = 'create_notice';
+export const DELETE_NOTICE = 'delete_notice';
 export const FETCH_NEWS = 'fetch_News';
 export const FETCH_NEW = 'fetch_New';
 export const CREATE_LOGIN = 'create_login';
@@ -27,13 +28,34 @@ export function fetchNotice(id) {
 }
 
 export function createNotice(values, callback) {
-    const request = axios.post(`${ROOT_URL}/notice${API_KEY}`, values)
-        .then(() => callback());
-    
-    return {
-        type: CREATE_NOTICE,
-        payload: request 
-    }
+  //파일, 이미지 여러개 처리 필요.
+  var formData = new FormData();
+  values.foreach(data=> {
+      formData.append(data.key, data);
+  })
+  const request = axios.post(`${ROOT_URL}/notice${API_KEY}`, formData,
+      {
+          headers: {
+          'Content-Type': 'multipart/form-data'
+          }
+      }
+  )
+      .then(() => callback());
+  console.log(values.attached)
+
+  return {
+      type: CREATE_NOTICE,
+      payload: request
+  }
+}
+
+export function deleteNotice(id) {
+  const request = axios.delete(`${ROOT_URL}/notice/${id}${API_KEY}`)
+      .then(() => callback());
+  return {
+      type: DELETE_NOTICE,
+      payload: id
+  }
 }
 
 export function fetchNews() {
--- CSE_frontend/src/index.js
+++ CSE_frontend/src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import MainNavigateBar from './components/MainNavigateBar';
 import MainPage from './components/MainPage';
 import NoticeCreate from './components/NoticeCreate';
+import NoticeUpdate from './components/NoticeUpdate';
 import NoticeDetail from './components/NoticeDetail';
 import NoticeList from './components/NoticeList';
 import Login from './components/Login';
@@ -24,6 +25,7 @@ ReactDOM.render(
         <Switch> 
           <Route path="/notice/new" component={NoticeCreate} />
           <Route path="/notice/:id" component={NoticeDetail} />
+          {/* <Route path="/notice/:id/update" component={NoticeUpdate} /> */}
           <Route path="/notice" component={NoticeList} />
           <Route path="/sign_in" component={Login} />
           <Route path="/" component={MainPage} />
