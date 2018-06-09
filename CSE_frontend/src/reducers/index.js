81eea053790af6ff831d1aee633a30f6cfac81d2
dongyeob <logan825@naver.com>
2018-06-09 23:05:33 +0900

    course changed

--- CSE_frontend/src/reducers/index.js
+++ CSE_frontend/src/reducers/index.js
@@ -1,19 +1,21 @@
 import { combineReducers } from 'redux';
 import NoticesReducer from './reducer_notices';
 import NewsReducer from './reducer_news';
-import MembersReducer from './reducer_members';
+import ProfessorsReducer from './reducer_professors';
 import TagsReducer from './reducer_tags';
-import ReservationReducer from './reducer_reservatoin';
+import UndercoursesReducer from './reducer_undercourses';
+import ResearchlabsReducer from './reducer_researchlabs';
 import { reducer as formReducer } from 'redux-form';
 
 
 const rootReducer = combineReducers({
   notices: NoticesReducer,
   news: NewsReducer,
-  members: MembersReducer,
-  reservation: ReservationReducer,
+  professors: ProfessorsReducer,
   form: formReducer,
   tags: TagsReducer,
+  undercourses: UndercoursesReducer,
+  researchlabs: ResearchlabsReducer,
 });
 
 export default rootReducer;
